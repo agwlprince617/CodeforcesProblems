dcc6ff76884e46de4db10238e95029943003c5ad
agwlprince617 <agrawalprince617@gmail.com>
2021-12-08 12:19:42 +0530

    first commit

---
+++ Watermelon.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+     ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt","r", stdin);
+    //     freopen("output.txt","w", stdout);
+    // #endif
+    int n;
+    cin>>n;
+    if(n==2) cout<<"NO"<<endl;
+    else if(n%2==0) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}    
